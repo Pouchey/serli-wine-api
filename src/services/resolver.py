a81d866ced4eca6580a525308e6725cb34d88151
Théophile <theophilerouchet@gmail.com>
2023-01-18 23:27:06 +0100

    test with minimal config

--- src/services/resolver.py
+++ src/services/resolver.py
@@ -1,30 +1,30 @@
 # import OCRs
-from src.services.OCR.KERAS.KERAS import KERAS
+# from src.services.OCR.KERAS.KERAS import KERAS
 from src.services.OCR.EASYOCR.EASYOCR import EASYOCR
-from src.services.OCR.PADDLEOCR.PADDLEOCR import PADDLEOCR
+# from src.services.OCR.PADDLEOCR.PADDLEOCR import PADDLEOCR
 
 # import NLPs
-from src.services.NLP.GENSIM.GENSIM import GENSIM
+# from src.services.NLP.GENSIM.GENSIM import GENSIM
 from src.services.NLP.TFIDF.TFIDF import TFIDF
 
 # get ocr by name
 def getOCR(name):
-    if name == "KERAS":
-        return KERAS()
-    elif name == "EASYOCR":
+    # if name == "KERAS":
+    #     return KERAS()
+    if name == "EASYOCR":
         return EASYOCR()
-    elif name == "PADDLEOCR":
-        return PADDLEOCR()
+    # elif name == "PADDLEOCR":
+    #     return PADDLEOCR()
 
 # get nlp by name
 def getNLP(name):
-    if name == "GENSIM":
-        return GENSIM(data_path="./ressources/external_data.csv", model_path="./src/services/models/gensim.model")
-    elif name == "TFIDF":
+    # if name == "GENSIM":
+    #     return GENSIM(data_path="./ressources/external_data.csv", model_path="./src/services/models/gensim.model")
+    if name == "TFIDF":
         return TFIDF(data_path="./ressources/data_clean.csv")
 
 def resolveImage(image):
-    OCR = getOCR("PADDLEOCR")
+    OCR = getOCR("EASYOCR")
     OCR_prediction = OCR.predict(image)
     
     NLP = getNLP("TFIDF")
