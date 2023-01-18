e387faf5f414499fbbc08704929f34cb8e72eb33
Théophile <theophilerouchet@gmail.com>
2023-01-18 16:31:44 +0100

    try removing some packages

--- src/services/resolver.py
+++ src/services/resolver.py
@@ -1,16 +1,15 @@
-from src.services.OCR.KERAS.KERAS import KERAS
-# from src.services.OCR.EASYOCR.EASYOCR import EASYOCR
-from src.services.NLP.GENSIM.GENSIM import GENSIM
-# from src.services.NLP.TFIDF.TFIDF import TFIDF
-# import cv2
+# from src.services.OCR.KERAS.KERAS import KERAS
+from src.services.OCR.EASYOCR.EASYOCR import EASYOCR
+# from src.services.NLP.GENSIM.GENSIM import GENSIM
+from src.services.NLP.TFIDF.TFIDF import TFIDF
 
 def resolveImage(image):
-    ocr = KERAS()
-    # ocr = EASYOCR()
+    # ocr = KERAS()
+    ocr = EASYOCR()
     OCR_prediction = ocr.predict(image)
     
-    nlp = GENSIM(data_path="./ressources/external_data.csv", model_path="./src/services/models/gensim.model")
-    # nlp = TFIDF(data_path="./ressources/data_clean.csv")
+    # nlp = GENSIM(data_path="./ressources/external_data.csv", model_path="./src/services/models/gensim.model")
+    nlp = TFIDF(data_path="./ressources/data_clean.csv")
     prediction = nlp.predict(OCR_prediction)
 
     print(prediction)
