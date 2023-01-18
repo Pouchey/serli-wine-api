c0d2187de1e697c2a610f5080b9d8b273ce8c446
SauzeauYannis <114395658+YannisSerli@users.noreply.github.com>
2023-01-18 14:43:44 +0100

    TFIDF

---
+++ src/services/NLP/TFIDF/TFIDF.py
@@ -0,0 +1,36 @@
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# import NLP class
+from src.services.NLP.NLP import NLP
+
+
+class TFIDF(NLP):
+    def __init__(self, model_path=None, data_path="../../data/data.csv"):
+        # call the super class constructor
+        super().__init__(model_path, data_path)
+        self.values = []
+        self.vectorizer = TfidfVectorizer(strip_accents='ascii')
+
+        if data_path is not None:
+            self.loadValues()
+            self.vectorizer.fit(self.values)
+
+    def predict(self, text):
+        cosine_similarity = cosine_similarity(self.vectorizer.transform(
+            [text]), self.vectorizer.transform(self.values)).flatten()
+
+        result = []
+
+        for index in cosine_similarity.argsort()[-4:][::-1]:
+            self.debug(round(cosine_similarity[index] * 100, 2), "% ===>", self.data[index])
+            result.append([self.data[index], round(cosine_similarity[index] * 100, 2)])
+
+        return result
+
+    def loadValues(self):
+        self.debug("[INFO] loading values from path:", self.data_path, "...")
+
+        for data in self.data:
+            line = data.split(',')
+            self.values.append(' '.join(line[1:]))
